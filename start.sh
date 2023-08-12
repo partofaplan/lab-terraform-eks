e27665cc8aa34166655d768f1b943605fdc2815c
Zach Perkins <partofaplan@gmail.com>
2023-08-11 20:32:15 -0500

    commenting out for test

--- start.sh
+++ start.sh
@@ -9,17 +9,17 @@
 # echo "Start Terraform apply..."
 # terraform apply -auto-approve
 
-echo "Update kubeconfig..."
-aws eks update-kubeconfig --name $(aws eks list-clusters | jq -r '.[] |.[]')
+# echo "Update kubeconfig..."
+# aws eks update-kubeconfig --name $(aws eks list-clusters | jq -r '.[] |.[]')
 
-echo "Add Jenkins helm repo..."
-helm repo add jenkins https://charts.jenkins.io
+# echo "Add Jenkins helm repo..."
+# helm repo add jenkins https://charts.jenkins.io
 
-echo "Update helm repos..."
-helm repo update
+# echo "Update helm repos..."
+# helm repo update
 
-echo "Switch to Jenkins directory..."
-cd ../lab-helm-jenkins
+# echo "Switch to Jenkins directory..."
+# cd ../lab-helm-jenkins
 
 echo "Deploy Jenkins..."
 helm upgrade --install myjenkins -f values.yaml jenkins/jenkins
