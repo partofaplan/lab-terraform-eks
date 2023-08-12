c43b1ed135453332d2eddb83617f98d408df5b25
Zach Perkins <partofaplan@gmail.com>
2023-08-11 20:18:30 -0500

    Updated start sh script

--- start.sh
+++ start.sh
@@ -1,10 +1,25 @@
 #!/bin/bash
 
-echo "Initializing Terrform..."
-terraform init
+# echo "Initializing Terrform..."
+# terraform init
 
-echo "Start Terraform plan..."
-terraform plan
+# echo "Start Terraform plan..."
+# terraform plan
 
-echo "Start Terraform apply..."
-terraform apply -auto-approve
+# echo "Start Terraform apply..."
+# terraform apply -auto-approve
+
+echo "Update kubeconfig..."
+aws eks update-kubeconfig --name $(aws eks list-clusters | jq -r '.[] |.[]')
+
+echo "Add Jenkins helm repo..."
+helm repo add jenkins https://charts.jenkins.io
+
+echo "Update helm repos..."
+helm repo update
+
+echo "Switch to Jenkins directory..."
+cd ../lab-helm-jenkins
+
+echo "Deploy Jenkins..."
+helm upgrade --install myjenkins -f values.yaml jenkins/jenkins
