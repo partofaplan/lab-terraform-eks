5dae5f4d9ba2336b94fe8652fdf8ebaf8e1a391c
Zach Perkins <partofaplan@gmail.com>
2023-08-12 14:36:17 -0500

    Modifying the start.sh

--- start.sh
+++ start.sh
@@ -1,25 +1,34 @@
 #!/bin/bash
 
-echo "Initializing Terrform..."
-terraform init
+# echo "Initializing Terrform..."
+# terraform init
 
-echo "Start Terraform plan..."
-terraform plan
+# echo "Start Terraform plan..."
+# terraform plan
 
-echo "Start Terraform apply..."
-terraform apply -auto-approve
+# echo "Start Terraform apply..."
+# terraform apply -auto-approve
 
-echo "Update kubeconfig..."
-aws eks update-kubeconfig --name $(aws eks list-clusters | jq -r '.[] |.[]')
+# echo "Update kubeconfig..."
+# aws eks update-kubeconfig --name $(aws eks list-clusters | jq -r '.[] |.[]')
 
-echo "Add Jenkins helm repo..."
-helm repo add jenkins https://charts.jenkins.io
+# echo "Add Jenkins helm repo..."
+# helm repo add jenkins https://charts.jenkins.io
 
-echo "Update helm repos..."
-helm repo update
+# echo "Update helm repos..."
+# helm repo update
 
-echo "Switch to Jenkins directory..."
-cd ../lab-helm-jenkins
+# echo "Switch to Jenkins directory..."
+# cd ../lab-helm-jenkins
 
-echo "Deploy Jenkins..."
-helm upgrade --install myjenkins -f values.yaml jenkins/jenkins
+# echo "Deploy Jenkins..."
+# helm upgrade --install myjenkins -f values.yaml jenkins/jenkins
+
+echo "Kick off the Github Action pipeline for building the ECR..."
+curl --request POST \
+  --url 'https://api.github.com/repos/partofaplan/weathervane-py/dispatches' \
+  --header 'Authorization: token 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' \
+  --data '{"event_type": "hello"}'
+
+echo "Deploy weather app to EKS..."
+helm upgrade --install weathervane ../weathervane-py/weathervane-py-charts/weathervane-py/
