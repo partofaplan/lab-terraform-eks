a2ea7d7f2b7ca74d3534282fa3d55698f1ded42c
Zach Perkins <partofaplan@gmail.com>
2023-08-12 15:06:53 -0500

    Updating progress

--- start.sh
+++ start.sh
@@ -1,33 +1,33 @@
 #!/bin/bash
 
-# echo "Initializing Terrform..."
-# terraform init
+echo "Initializing Terrform..."
+terraform init
 
-# echo "Start Terraform plan..."
-# terraform plan
+echo "Start Terraform plan..."
+terraform plan
 
-# echo "Start Terraform apply..."
-# terraform apply -auto-approve
+echo "Start Terraform apply..."
+terraform apply -auto-approve
 
-# echo "Update kubeconfig..."
-# aws eks update-kubeconfig --name $(aws eks list-clusters | jq -r '.[] |.[]')
+echo "Update kubeconfig..."
+aws eks update-kubeconfig --name $(aws eks list-clusters | jq -r '.[] |.[]')
 
-# echo "Add Jenkins helm repo..."
-# helm repo add jenkins https://charts.jenkins.io
+echo "Add Jenkins helm repo..."
+helm repo add jenkins https://charts.jenkins.io
 
-# echo "Update helm repos..."
-# helm repo update
+echo "Update helm repos..."
+helm repo update
 
-# echo "Switch to Jenkins directory..."
-# cd ../lab-helm-jenkins
+echo "Switch to Jenkins directory..."
+cd ../lab-helm-jenkins
 
-# echo "Deploy Jenkins..."
-# helm upgrade --install myjenkins -f values.yaml jenkins/jenkins
+echo "Deploy Jenkins..."
+helm upgrade --install myjenkins -f values.yaml jenkins/jenkins
 
 echo "Kick off the Github Action pipeline for building the ECR..."
 curl --request POST \
   --url 'https://api.github.com/repos/partofaplan/weathervane-py/actions/workflows/66039728/dispatches' \
-  --header 'Authorization: Bearer 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' \
+  --header 'Authorization: token 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' \
   --header 'X-GitHub-Api-Version: 2022-11-28' \
   --data '{"ref":"main"}'
 
