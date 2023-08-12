6d3b7002584aadd00a1465871259e9a8cb291063
Zach Perkins <partofaplan@gmail.com>
2023-08-12 12:46:35 -0500

    Trying to add ECR

--- main.tf
+++ main.tf
@@ -110,3 +110,16 @@ resource "aws_eks_addon" "ebs-csi" {
     "terraform" = "true"
   }
 }
+
+resource "aws_ecr_repository" "ecr" {
+  for_each             = toset(var.ecr_name)
+  name                 = each.key
+  image_tag_mutability = var.image_mutability
+  encryption_configuration {
+    encryption_type = var.encrypt_type
+  }
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+  tags = var.tags
+}
